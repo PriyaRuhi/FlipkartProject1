6d295952a2bfc77b663929c7578c8f2d83ebf3af
PriyaRuhi <priyaaher23@gmail.com>
2021-10-01 18:28:51 +0530

    By Ruhi

---
+++ src/test/java/cucumbermap/logout.java
@@ -0,0 +1,17 @@
+package cucumbermap;
+
+public class logout 
+{
+   public void log()
+   {
+	   System.out.println("*********");
+   }
+
+public static void main(String[]args)
+{
+	logout obj=new logout();
+	obj.log();
+}
+
+}
+
